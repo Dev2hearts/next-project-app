88d2506f126d59323f582e05c90568e89f5efaae
dev2hearts <js.park119de@gmail.com>
2023-11-01 17:19:02 +0900

    test commit

--- pages/404.tsx
+++ pages/404.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const PageNotFound = () => {
   return (
     <div>
-      <h1>페이지를 찾을수 없습니다.</h1>
+      <h1>페이지를 찾을수 없습니다.
+        {/* <span>test 입니다.</span> */}
+      </h1>
     </div>
   );
 };
