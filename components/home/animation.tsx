7b556ec041d101f6038c807e3b41e60d725e319c
dev2hearts <js.park119de@gmail.com>
2024-08-07 21:36:55 +0900

    lottie 라이브러리 ssr 수정함

--- components/home/animation.tsx
+++ components/home/animation.tsx
@@ -3,6 +3,11 @@ import Lottie from 'react-lottie-player';
 
 import lottieJson from '@/public/animation_lmka1e89.json';
 
+import dynamic from 'next/dynamic';
+
 export default function Animation() {
+  const Lottie = dynamic(() => import('react-lottie-player'), {
+    ssr: false,
+  });
   return <Lottie loop animationData={lottieJson} play />;
 }
